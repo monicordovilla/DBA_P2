81532a7d9f491b3a3539aacc5feebbf009cee451
Kieran <K-Embee@protonmail.com>
2019-10-10 13:32:48 +0200

    Creado plantilla basica de la clase a partir de lo dado en el seminario 2

---
+++ DBAPrac2/src/dbaprac2/AgenteSimple.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbaprac2;
+
+import es.upv.dsic.gti_ia.core.ACLMessage;
+import es.upv.dsic.gti_ia.core.AgentID;
+import DBA.SuperAgent;
+
+/**
+ *
+ * @author Kieran
+ */
+public class AgenteSimple extends SuperAgent{
+    public AgenteSimple(AgentID aid) throws Exception {
+        super(aid);
+    }
+    
+    private void comunicar(String nombre, String mensaje) {
+        ACLMessage outbox = new ACLMessage();
+        outbox.setSender(this.getAid());
+        outbox.setReceiver(new AgentID(nombre));
+        outbox.setContent(mensaje);
+        this.send(outbox);
+    }
+    
+    private String escuchar() {
+        ACLMessage inbox;
+        try{
+            inbox = this.receiveACLMessage();
+        }
+        catch(Exception e){
+            System.out.println("Error de comunicación: Excepción al escuchar");
+            return "ERROR";
+        }
+        String mensaje = inbox.getContent();
+        System.out.println("Mensaje recibido:\n" + mensaje);
+        return mensaje;
+    }
+    
+    @Override
+    public void init() { //Opcional
+        System.out.println("\nInicializado");
+    }
+    
+    @Override
+    public void execute() {
+        comunicar("nombre", "mensaje");
+    }
+    
+    @Override
+    public void finalize() { //Opcional
+        System.out.println("\nFinalizando");
+        super.finalize(); //Pero si se incluye, esto es obligatorio
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DBA_P2
 Practica 2 de la asignatura de Desarrollo Basado en Agentes
+
+Estan unicamente los ficheros fuente en la repo, para crear el proyecto en netbeans podeis crear proyecto desde fuente, e incluir las clases del profesor (magentix, SuperAgent, libreria JSON) como librerias.
